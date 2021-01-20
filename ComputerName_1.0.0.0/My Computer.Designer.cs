68563857def4909d49af632e849d046ad2e9f9bf
darkish101 <hoalnowiser@hotmail.com>
2021-01-20 21:50:28 +0300

    commiting the program

---
+++ ComputerName_1.0.0.0/My Computer.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace ComputerName_1._0._0._0
+{
+    partial class My_Computer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(My_Computer));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOSName = new System.Windows.Forms.Label();
+            this.lable2 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblCompName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblIPaddress = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(174, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OS:";
+            // 
+            // lblOSName
+            // 
+            this.lblOSName.AutoSize = true;
+            this.lblOSName.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOSName.Location = new System.Drawing.Point(266, 111);
+            this.lblOSName.Name = "lblOSName";
+            this.lblOSName.Size = new System.Drawing.Size(116, 29);
+            this.lblOSName.TabIndex = 1;
+            this.lblOSName.Text = "OSName";
+            // 
+            // lable2
+            // 
+            this.lable2.AutoSize = true;
+            this.lable2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lable2.Location = new System.Drawing.Point(76, 271);
+            this.lable2.Name = "lable2";
+            this.lable2.Size = new System.Drawing.Size(153, 29);
+            this.lable2.TabIndex = 2;
+            this.lable2.Text = "User Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 359);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Computer Name:";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Location = new System.Drawing.Point(266, 271);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(137, 29);
+            this.lblUserName.TabIndex = 4;
+            this.lblUserName.Text = "UserName";
+            // 
+            // lblCompName
+            // 
+            this.lblCompName.AutoSize = true;
+            this.lblCompName.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompName.Location = new System.Drawing.Point(266, 359);
+            this.lblCompName.Name = "lblCompName";
+            this.lblCompName.Size = new System.Drawing.Size(152, 29);
+            this.lblCompName.TabIndex = 5;
+            this.lblCompName.Text = "CompName";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(161, 87);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(179, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "IP:";
+            // 
+            // lblIPaddress
+            // 
+            this.lblIPaddress.AutoSize = true;
+            this.lblIPaddress.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIPaddress.Location = new System.Drawing.Point(266, 192);
+            this.lblIPaddress.Name = "lblIPaddress";
+            this.lblIPaddress.Size = new System.Drawing.Size(162, 29);
+            this.lblIPaddress.TabIndex = 8;
+            this.lblIPaddress.Text = "lblIPaddress";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(602, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = ":نظام التشغيل";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(604, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 29);
+            this.label5.TabIndex = 10;
+            this.label5.Text = ":عنوان الجهاز";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(602, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(195, 29);
+            this.label6.TabIndex = 11;
+            this.label6.Text = ":اسم المستخدم";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(604, 359);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(177, 29);
+            this.label7.TabIndex = 12;
+            this.label7.Text = ":اسم الكمبيوتر";
+            // 
+            // My_Computer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(509, 447);
+            this.ControlBox = false;
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblIPaddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblCompName);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lable2);
+            this.Controls.Add(this.lblOSName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "My_Computer";
+            this.ShowInTaskbar = false;
+            this.Text = "My Computer";
+            this.Load += new System.EventHandler(this.Computer_Name_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOSName;
+        private System.Windows.Forms.Label lable2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblCompName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblIPaddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ ComputerName_1.0.0.0/My Computer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerName_1._0._0._0
+{
+    public partial class My_Computer : Form
+    {
+        public My_Computer()
+        {
+            InitializeComponent();
+        }
+
+        [Obsolete]
+        private void Computer_Name_Load(object sender, EventArgs e)
+        {
+            Environment.CurrentDirectory = Environment.GetEnvironmentVariable("windir");
+            DirectoryInfo info = new DirectoryInfo(".");
+
+            lblOSName.Text = info.Name;
+            lblUserName.Text = Environment.UserName;
+            lblCompName.Text = Environment.MachineName;
+            //var UDN = Environment.UserDomainName;
+            lblIPaddress.Text = Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString();
+        }
+    }
+}
